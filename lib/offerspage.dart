b32f1b031610af824b2f8730a680d41af311cda4
RafaelVatista <rafaelbatista9710@gmail.com>
2023-12-05 16:38:46 +0000

    Starting of app dev

---
+++ lib/offerspage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class OffersPage extends StatelessWidget {
+  const OffersPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class Offer extends StatelessWidget {
+  const Offer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
